df53b01afac6207bdabaaca33ba2fd57cc96648f
Prince Verma <princeverma.jobs@gmail.com>
2025-03-24 13:54:27 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (78.15%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        // Create the first elem inner list and add 1 to it
+        List<Integer> firstelem_innerList = new ArrayList<>();
+        firstelem_innerList.add(1);
+        ans.add(firstelem_innerList);
+
+
+
+        
+        // here we use < because numRows is not zero based indexing
+        for (int row = 1; row < numRows; row++) 
+        {
+            ans.add(findRow(row)); // row is zero based indexing
+        }
+        return ans;
+    }
+
+    
+    public List<Integer> findRow(int row)  
+    {       
+        long elem = 1; 
+        List<Integer> tempList = new ArrayList<>();
+        tempList.add(1);                               
+        for(int col=1 ; col<=row; col++)// because row is zero based indexing
+        {
+            elem = elem * (row-col +1);
+            elem = elem / col;
+
+            tempList.add((int)elem);
+        }
+        return tempList;
+    }
+}
