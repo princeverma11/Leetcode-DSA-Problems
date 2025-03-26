192bad0373631b63f5053606725f59bf42ac9805
Prince Verma <princeverma.jobs@gmail.com>
2025-03-26 16:18:49 +0530

    Time: 2 ms (29.33%) | Memory: 57.5 MB (35.56%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,33 @@
+
+class Solution {
+    public void rotate(int[] nums, int k) 
+    {
+        int[] arr = nums;
+        int[] temp = new int[2*arr.length];
+        int n= arr.length;
+
+        
+        // so now net_rotations will be always <= arr.length
+        int net_right_rotations = k % arr.length;  
+
+        int lr = n - net_right_rotations; // lr = left_rotaioons
+        
+
+         for (int i = 0; i <= n - 1; i++) {
+            temp[i] = arr[i];
+            temp[i + arr.length] = arr[i];
+        }
+        
+
+           
+        
+        for( int i=0 ; i<arr.length ; i++)
+        {
+            arr[i] = temp[i+lr];   
+        }
+            
+        
+        
+    }
+        
+    }
