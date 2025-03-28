13a0805696113ee64c8fb2f375648db77cb2b732
Prince Verma <princeverma.jobs@gmail.com>
2025-03-28 13:10:57 +0530

    Time: 1 ms (99.80%) | Memory: 58.4 MB (19.44%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,46 +1,77 @@
-// this is sliding window technique
-
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        int l=0;
+        int r=0;
+        int currsum = 0;
+        int minLength = Integer.MAX_VALUE;
         
-        //preprocessing
-        int sum = 0; 
-        int[] cs = new int[nums.length];
-        for(int i=0 ; i<nums.length ; i++)
+        
+
+        while(r<nums.length)
         {
-            sum = sum+nums[i];
-            cs[i] = sum; 
+            currsum = currsum + nums[r];
+           
+            while(currsum>=target)
+            {
+                 minLength = Math.min(minLength, r-l+1);
+                currsum = currsum - nums[l];
+                l++;
+            } 
+            r++;
         }
 
-        int i = 0;
-        int j = 0;
-        int currentsum = 0;
-        int minsum = Integer.MAX_VALUE;
-        int minLength = Integer.MAX_VALUE;
-        while(i<nums.length && j<nums.length )
-        {
-            currentsum = cs[j] - (i>0?cs[i-1]:0);
-            
-            if(currentsum >= target)
-            {
-                minsum = Math.min(minsum , currentsum);
-                minLength = Math.min(minLength , j-i+1);
-                if(i==j)j++;
-                else i++;
-            }
-            if(currentsum< target)
-            {
-                j++;
-            }
-            
+         if(minLength == Integer.MAX_VALUE) {
+            minLength = 0;
         }
-        if(minLength == Integer.MAX_VALUE)
-        {
-            return 0;
-        }
-        else
-        {
-            return minLength;
-        }
+       
+
+        return minLength;
+
+
     }
 }
+
+// class Solution {
+//     public int minSubArrayLen(int target, int[] nums) {
+        
+//         //preprocessing
+//         int sum = 0; 
+//         int[] cs = new int[nums.length];
+//         for(int i=0 ; i<nums.length ; i++)
+//         {
+//             sum = sum+nums[i];
+//             cs[i] = sum; 
+//         }
+
+//         int i = 0;
+//         int j = 0;
+//         int currentsum = 0;
+//         int minsum = Integer.MAX_VALUE;
+//         int minLength = Integer.MAX_VALUE;
+//         while(i<nums.length && j<nums.length )
+//         {
+//             currentsum = cs[j] - (i>0?cs[i-1]:0);
+            
+//             if(currentsum >= target)
+//             {
+//                 minsum = Math.min(minsum , currentsum);
+//                 minLength = Math.min(minLength , j-i+1);
+//                 if(i==j)j++;
+//                 else i++;
+//             }
+//             if(currentsum< target)
+//             {
+//                 j++;
+//             }
+            
+//         }
+//         if(minLength == Integer.MAX_VALUE)
+//         {
+//             return 0;
+//         }
+//         else
+//         {
+//             return minLength;
+//         }
+//     }
+// }
