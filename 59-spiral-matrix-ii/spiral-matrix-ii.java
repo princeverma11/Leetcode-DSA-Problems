4c938f2b7aa03a53e2a12c901c0aa19026c36099
Prince Verma <princeverma.jobs@gmail.com>
2025-04-03 14:06:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (65.62%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -2,40 +2,42 @@ class Solution {
     public int[][] generateMatrix(int n) {
         
         int[][] ans = new int[n][n];
+
+        int flag = 1;
+
         int top = 0;
+        int left = 0;
         int right = n-1;
         int bottom = n-1;
-        int left = 0;
-        int index = 1;
-        while(left<=right && top<=bottom)
+
+        while(top<=bottom && left<=right)
         {
-            for(int i=left ; i<=right ;i++)
+            for(int i=left ; i<=right ; i++)
             {
-                ans[top][i] = index;
-                index++;
+                ans[top][i] = flag;
+                flag++;
             }
+            for(int i = top+1 ; i<=bottom ; i++)
+            {
+                ans[i][right] = flag;
+                flag++;
+            }
+            for(int i= right-1 ; i>=left ; i--)
+            {
+                ans[bottom][i] = flag;
+                flag++;
+            }
+            for(int i = bottom-1 ; i>=top+1 ; i--)
+            {
+                ans[i][left] = flag;
+                flag++;
+            }
+
             top++;
-            
-            for(int i=top;i<=bottom ; i++)
-            {
-                ans[i][right] = index;
-                index++;
-            }
             right--;
-
-            for(int i=right ; i>=left ; i--)
-            {
-                ans[bottom][i] = index;  
-                index++;
-            }
             bottom--;
-
-            for(int i=bottom;i>=top;i--)
-            {
-                ans[i][left] = index;
-                index++;
-            }
             left++;
+
 
 
         }
