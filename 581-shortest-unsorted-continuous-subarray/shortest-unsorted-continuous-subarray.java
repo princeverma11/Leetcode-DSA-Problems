d43d703af9858289b9db1d870b81857161045bed
Prince Verma <princeverma.jobs@gmail.com>
2025-04-04 17:24:05 +0530

    Time: 1 ms (96.69%) | Memory: 45.8 MB (13.62%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -5,20 +5,27 @@ class Solution {
      int startindex =-1;
      int endindex =-1;
 
+    
+    
+    // Step 1: Find the starting index where the order breaks
      for(int i=0 ; i<n-1 ; i++)
      {
         if(nums[i+1]<nums[i])
         {
              startindex =i;
-             break; 
+             break; // We only need the first break point
         }
      }
 
-     if(startindex == -1) // if array is already sorted
+
+     // If no break point found, array is already sorted  
+     if(startindex == -1) 
      {
         return 0;
      }
+     
 
+     // Step 2: Find the ending index where the order breaks
      for(int i=n-1 ; i>0 ; i--)
      {
         if(nums[i-1]>nums[i])
@@ -27,7 +34,9 @@ class Solution {
              break;
         }
      }
+     
 
+     // Step 3: Find the min and max in the unsorted subarray we've identified
      int min = Integer.MAX_VALUE;
      int max = Integer.MIN_VALUE;
      for(int i = startindex ; i<=endindex ; i++)
@@ -37,25 +46,31 @@ class Solution {
 
      }
 
+
+     // Step 4: Expand the startindex to left if needed
+     // Check if any element before startindex is greater than our mi
      for(int i=0 ; i<startindex ; i++)
      {
         if(nums[i] > min)
         {
-            startindex = i;
-            break;
+            startindex = i; // Need to include this element in our subarray
+            break;  // We only need the first such element
         }
      }
+     
 
+     // Step 5: Expand the endindex to right if needed
+     // Check if any element after endindex is smaller than our ma
      for(int i=n-1 ; i>endindex ;i--)
      {
         if(nums[i]<max)
         {
-            endindex = i;
-            break;
+            endindex = i; // Need to include this element in our subarray
+            break; // We only need the first such element
         }
      }
 
-
-    return endindex-startindex+1;
+     // Return the length of the unsorted subarray 
+     return endindex-startindex+1;
     }
 }
