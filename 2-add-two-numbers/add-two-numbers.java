9e71abed70ae624a42b5ad2cf174489898b813da
Prince Verma <princeverma.jobs@gmail.com>
2025-05-01 16:31:38 +0530

    Time: 2 ms (12.87%) | Memory: 44.9 MB (13.80%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -11,75 +11,41 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        ListNode num1 = l1;
-        ListNode num2 = l2;
-        
-        
-        
-         // Find the length of both the linked lists
-        int len1 = countNodes(num1);
-        int len2 = countNodes(num2);
-        
-        // If num1 is smaller, swap the two linked lists by
-        // calling the function with reversed parameters
-        if (len1 < len2) {
-            return addTwoNumbers(num2, num1);
-        }
-        
-        
-        ListNode res = num1; //kyuki rnum1 is longer
-        
-        ListNode d1 = num1 , d2 = num2; // d represents digit
-        int carry =0;
-        
-        while(d2 != null || carry!= 0)
+        ListNode ansHead = new ListNode();
+        ListNode anstemp = null;
+
+        ListNode l1temp = l1;
+        ListNode l2temp = l2;
+        int carry = 0;
+        while(l1temp != null || l2temp != null || carry != 0)
         {
-            // Add carry to d1
-            d1.val += carry;
-            
-            if(d2!=null)   // If d2 is not empty, add it to d1
+            int sum = carry;
+            if(l1temp!=null)
             {
-                d1.val += d2.val;
-                d2 = d2.next;
+                sum += l1temp.val;
+                l1temp = l1temp.next;
             }
+            if(l2temp!=null)
+            {
+                sum += l2temp.val;
+                l2temp = l2temp.next;
+            }
+            carry = sum/10;
+            int digit = sum%10;
 
-         // Store the carry for the next nodes
-        carry = d1.val / 10;
-        
-        // Store the remainder in num1
-        d1.val = d1.val % 10;
-        
-        // If we are at the last node of d1 but carry is
-        // still left, then add a new node to d1
-        if (d1.next == null && carry != 0) 
-        {
-            d1.next = new ListNode(0);
+            ListNode digitNode = new ListNode(digit);
+            if(anstemp == null)
+            {
+                ansHead = digitNode;
+                anstemp = digitNode;
+            }
+            else
+            {
+                anstemp.next = digitNode;
+                anstemp = digitNode;
+            }
         }
-        d1 = d1.next;        
-        }
-        
-        // Reverse the resultant linked list to get the
-        // required linked list
-        return res;
+        return ansHead;
     }
-    
-
-    
-    
-    static int countNodes(ListNode head) 
-    {
-        int len = 0;
-        ListNode curr = head;
-
-        while (curr != null) {
-            len += 1;
-            curr = curr.next;
-        }
-        return len;
-    }
-
-
-
-
 
 }
