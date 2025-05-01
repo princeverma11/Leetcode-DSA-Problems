7d3c33b0306421b6a288e200c23a0cfde867a99b
Prince Verma <princeverma.jobs@gmail.com>
2025-05-01 19:58:34 +0530

    Time: 2 ms (85.70%) | Memory: 48.5 MB (33.70%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -8,22 +8,19 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-///////////////////////////////////////////////////////////////////////
-// ignore gfg explanation because it solves by O(nsquare)
-// anuj ignores even length LL
-///////////////////////////////////////////////////////////////////////
 class Solution {
     public void reorderList(ListNode head) {
-     
-        ListNode middle = findMiddle(head);
         
-        ListNode revLLhead = reverseLL(middle.next);
-        middle.next = null; // Split the list into two halves
+
+        ListNode middle = middleNode(head); 
+        ListNode revHead = reverseList(middle.next);
+        middle.next = null;
         ListNode t1 = head;
-        ListNode t2 = revLLhead;
-       
-       while( t2 != null)
-       {
+        ListNode t2 = revHead;
+
+
+        while(t2 != null)
+        {
            ListNode temp_nextoft1 = t1.next;
            ListNode temp_nextoft2 = t2.next;
            t1.next = t2;
@@ -31,43 +28,34 @@ class Solution {
            
            t1 = temp_nextoft1;
            t2 = temp_nextoft2;
-           
-           
-       }
-       
-    }
-    
-    
-    static ListNode findMiddle(ListNode head)
-    {
-          ListNode slow = head ;
-          ListNode fast = head;
-    
-          while(fast != null && fast.next != null)
-          {
-             slow = slow.next;
-              fast = fast.next.next;
-          }
-           return slow;   
-    }
-    static ListNode reverseLL(ListNode head)
-    {
-        if(head ==null || head.next == null) return head;
-        ListNode beforecurr = null;
-        ListNode curr = head;
-        ListNode aftercurr;
-        
-        while(curr!=null)
-        {
-            aftercurr = curr.next;//yaha ac aage bd raha h
-            
-            curr.next = beforecurr;
-            
-            //moving forward
-            beforecurr = curr;
-            curr = aftercurr;
-            
         }
-        return beforecurr;
+    }
+
+
+public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null  )
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
+
+public ListNode reverseList(ListNode head) {
+        if(head ==null || head.next == null) return head;
+        ListNode curr = head;
+        ListNode after_curr = head.next;
+        ListNode before_curr = null;
+        while (curr != null) {
+            after_curr = curr.next;   
+            curr.next = before_curr;  
+            before_curr = curr;
+            curr = after_curr;
+        }
+        return before_curr;
     }
 }
