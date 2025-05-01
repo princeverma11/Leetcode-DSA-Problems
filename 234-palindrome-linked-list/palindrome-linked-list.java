34f9dda167a11337d6ade76922d510a2d4ac751c
Prince Verma <princeverma.jobs@gmail.com>
2025-05-01 12:36:29 +0530

    Time: 6 ms (46.84%) | Memory: 60.3 MB (66.32%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,56 +10,51 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-       ListNode middle = findMiddle(head);
-       ListNode headOfReverse = reverseRightPart(middle);
-       
-       ListNode t1 = head;
-       ListNode t2 = headOfReverse;
-       while(t2!=null)
-       {
-           if(t1.val != t2.val)
-           {
-               return false;
-           }
-           t1 = t1.next;
-           t2 = t2.next;
-       }
-       return true;
+        ListNode middle_node = middleNode(head);
+        ListNode reverse_head = reverseList(middle_node);
 
+        ListNode t1 = head;
+        ListNode t2 = reverse_head;
+        while(t2 != null)
+        {
+            if(t1.val == t2.val)
+            {
+                t1 = t1.next;
+                t2 = t2.next;
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
     }
-    
-    static ListNode findMiddle(ListNode head)
-    {
-         
+
+     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-
-        while(fast != null && fast.next != null)
+        while(fast != null && fast.next != null  )
         {
             slow = slow.next;
             fast = fast.next.next;
         }
         return slow;
-      
-    
     }
-    static ListNode reverseRightPart(ListNode middle)
-    {
-        ListNode curr = middle;
-        ListNode after_curr = middle.next;
+
+
+
+
+    public ListNode reverseList(ListNode head) {
+        if(head ==null || head.next == null) return head;
+        ListNode curr = head;
+        ListNode after_curr = head.next;
         ListNode before_curr = null;
-        while(curr!=null)
-        {
-            curr.next = before_curr;
-            
+         while (curr != null) {
+            after_curr = curr.next;        
+            curr.next = before_curr;     
             before_curr = curr;
             curr = after_curr;
-            if(after_curr!=null) // If `after_curr` is not null, move it to the next node in the original list
-            {
-                after_curr = after_curr.next;
-            }else{curr = null;} // If `after_curr` is null, set `curr` to null to exit the loop
-            
-            
         }
         return before_curr;
     }
