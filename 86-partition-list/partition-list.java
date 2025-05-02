c5211e8ba571f0a9685b289969e9d01dfdf54ce4
Prince Verma <princeverma.jobs@gmail.com>
2025-05-02 11:38:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (30.50%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -8,47 +8,29 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- ///////////////////////////////////////////////////////////////////////////////
- //SEE THIS SAME QUES ON GFG .. ITS THE SAME QUESTION BUT ITS SLIGHTLY MORE INTRESTING .. YOU WILL LIKE IT
-///////////////////////////
-
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        
-       ListNode small = new ListNode(-1);
-       
-       ListNode large = new ListNode(-1);
-       
-       
-        ListNode sHead = small;
-        ListNode sTail = small;
-        
-        ListNode lHead = large;
-        ListNode lTail = large;
-       
-       
-       
-       ListNode temp = head;
-       while (temp != null) {
-            if (temp.val < x) {
-                sTail.next = temp;
-                sTail = sTail.next;
-            }  
-            else {
-                lTail.next = temp;
-                lTail = lTail.next;
+        if(head== null || head.next == null)return head;
+        ListNode smallHead = new ListNode(-101);
+        ListNode smallTail = smallHead;
+
+        ListNode bigHead = new ListNode(-101);
+        ListNode bigTail = bigHead;
+
+        ListNode temp = head;
+        while(temp != null)
+        {
+            if(temp.val < x) {
+                smallTail.next = new ListNode(temp.val);
+                smallTail = smallTail.next;
+            } else {
+                bigTail.next = new ListNode(temp.val);
+                bigTail = bigTail.next;
             }
-            temp = temp.next;
+            temp = temp.next;  // Move to next node (MISSING IN YOUR CODE)
         }
-       
-       
-       
-       // Merge the three lists
-        sTail.next =  lHead.next;
-        lTail.next = null; // Important to avoid cycles
-        // kyuki last k element m jo aayega uske next m ho sakta h  koi element ho 
-        
-        return sHead.next;
-       
+        smallTail.next = bigHead.next;  // Connect small list to big list
+        return smallHead.next;           // Return the merged list
     }
+    
 }
