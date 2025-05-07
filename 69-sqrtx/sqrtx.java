cb821ebfb4d247737285a7320cc3121577317548
Prince Verma <princeverma.jobs@gmail.com>
2025-05-07 18:15:10 +0530

    Time: 1 ms (85.61%) | Memory: 40.8 MB (69.51%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,26 +1,33 @@
 class Solution {
     public int mySqrt(int x) {
-        int l = 0;
-        int r = x;
-        int ans = -1;
-
-        if (x == 0 || x == 1) {  // edge case handling
+       int l = 0;
+       int r = x;
+       int ans = -1;
+       if (x == 0 || x == 1) {  // edge case handling
             return x;
         }
+       while(l<=r)
+       {
+        int mid = l + (r-l)/2;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            
-             if (mid == x/mid) return mid;
-            // Avoid integer overflow erro by using mid < x / mid instead of mid * mid <= x
-            if (mid < x / mid) {
-                ans = mid; // Update the answer
-                l = mid + 1; // Search the right half
-            } else {
-                r = mid - 1; // Search the left half
-            }
+        
+        if(mid == x/mid)
+        {
+            return mid;
+        }
+        if(mid > x/mid)
+        {
+            r = mid-1;
         }
 
-        return ans;
+        if(mid < x/mid)
+        {
+            ans = mid;
+            l = mid+1;
+        }
+       }
+       
+       return ans;
+       
     }
 }
