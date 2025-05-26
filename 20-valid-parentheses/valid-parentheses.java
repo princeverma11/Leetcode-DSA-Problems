3c5adc2ce9fd28b340b2083cc19b03bed57279e4
Prince Verma <princeverma.jobs@gmail.com>
2025-05-26 10:32:59 +0530

    Time: 2 ms (97.23%) | Memory: 41.2 MB (99.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,23 +1,27 @@
-import java.util.ArrayDeque;
-
 class Solution {
     public boolean isValid(String s) {
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            }
-            else {
-                if(stack.isEmpty()) return false;
-                
-                char top = stack.pop();
-                if(c == ')' && top != '(') return false;
-                if(c == '}' && top != '{') return false;
-                if(c == ']' && top != '[') return false;
-            }
-        }
-        return stack.isEmpty();
+        Deque<Character> stack = new ArrayDeque<>();
+       
+       for(int i=0 ; i<s.length() ; i++)
+       {
+           char curr = s.charAt(i);
+           if(curr == '(' || curr == '{' || curr == '[')
+           {
+               stack.push(curr);
+           }
+           else
+           {
+               if(stack.isEmpty()) return false;
+               
+               char opening = stack.pop();
+               if((opening == '(' && curr == ')') || (opening == '{' && curr == '}') || (opening == '[' && curr == ']'))
+               {
+                   continue;
+               }
+               else return false;
+               
+           }
+       }
+       return stack.isEmpty();
     }
 }
