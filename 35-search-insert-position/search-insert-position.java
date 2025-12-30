5976815f45582f8748d2202c1f8071552b17f7c0
Prince Verma <princeverma.jobs@gmail.com>
2025-12-30 11:43:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (80.69%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,23 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int[] arr = nums;
-        int x = target;
+        int l = 0;
+        int r = nums.length-1;
+
         
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        
-        return left;
+
+        while(l<=r)
+        {
+            int m = l+(r-l)/2;
+
+            if(nums[m] == target)
+            return m;
+
+            else if( target>nums[m] )
+            l = m+1;
+
+            else
+            r = m-1;
+        } 
+        return l;
     }
 }
