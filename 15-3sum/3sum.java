0eda9a5aba885ec8c724ab1cef10f414554ee20a
Prince Verma <princeverma.jobs@gmail.com>
2026-04-01 12:58:44 +0530

    Time: 47 ms (18.03%) | Memory: 60.5 MB (6.82%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,66 +1,63 @@
-// video m solution jo anuj k karaya woh incomplete h, sare test cases pass nhi ho rhe the.
-
-// mene solution deepseek se nikalwaya h toh sare test cases pass ho rhe the.
-
-
-
-//link - https://leetcode.com/problems/3sum/
-
-//below is my complete solution with all test cases passing
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int[] arr = nums;
-        int n = arr.length;
-        
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(arr);
+        Arrays.sort(nums);
+        int n = nums.length;
 
-        for(int i=0 ; i<n-2; i++)
-        {
-            if(i>0 && arr[i] == arr[i-1])
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0 ; i<n-2 ; i++)
+         {
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             
-        int l = i+1;
-        int r = n-1;
 
-       
-
-        while(l<r)
-        {
-           
+            int l = i+1;
+            int r = n-1;
             
-            if(arr[l]+arr[r] == -arr[i])
+            while(l<r)
             {
                 List<Integer> temp = new ArrayList<>();
-                temp.add(arr[i]);
-                temp.add(arr[l]);
-                temp.add(arr[r]);
-                ans.add(temp);
-                l++;
-                r--;
-                while(l<r && arr[l] == arr[l-1])
+                if(nums[l] + nums[r] == -nums[i])
+                {
+                    
+                    
+                    temp.add(nums[i]);
+                    temp.add(nums[l]);
+                    temp.add(nums[r]); 
+
+                    ans.add(temp);
+                    
+                    l++;
+                    r--;
+                    
+
+                    while(l<r && nums[l] == nums[l-1])
+                    {
+                        l++;
+                        continue;
+                    }
+                    while(r>l && nums[r] == nums[r+1])
+                    {
+                        r--;
+                        continue;
+                    }
+
+                }
+                else if(nums[l] + nums[r] < -nums[i])
                 {
                     l++;
                 }
-                while(l<r && arr[r] == arr[r+1])
+                else
                 {
                     r--;
                 }
-            }
             
-            if(arr[l]+arr[r]<-arr[i])
-            {
-                l++;
-            }    
-            if(arr[l]+arr[r]>-arr[i])
-            {
-                r--;
             }
-            
-        
         }
-    }
-    return ans;
 
+     return ans;
     }
+     
+
 }
