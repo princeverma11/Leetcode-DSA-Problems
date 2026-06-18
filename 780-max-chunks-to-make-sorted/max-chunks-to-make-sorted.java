845bc85b27fc3802ae4f3e4528fbbaa1849353b5
Prince Verma <princeverma.jobs@gmail.com>
2026-06-18 08:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (28.34%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,14 +1,20 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int n = arr.length;
-        int maxsofar = arr[0];
-        int chunks=0;
+        
+        int n= arr.length;
+        
+        int totalchunks = 0;
+        int maxi = arr[0];
+
         for(int i=0 ; i<n ; i++)
         {
-            maxsofar = Math.max(maxsofar , arr[i]);
-            if(maxsofar == i)
-            chunks++;
+            maxi = Math.max(maxi , arr[i]);
+            if(maxi == i)
+            {
+                totalchunks++;
+            }
         }
-        return chunks;
+return totalchunks;
+    
     }
 }
